806e4f662188f9c0729c8b763b76f68f2b416e94
Ajay <gillajay.mit.edu>
2025-07-12 15:47:29 -0400

    Refactor code structure for improved readability and maintainability

--- src/model.py
+++ src/model.py
@@ -124,9 +124,9 @@ class Filament():
         dust_model = bridge_shape * dust_signal
         total_model = sz_model + dust_model
 
-        #return total_model, sz_model, dust_model
+        return total_model, sz_model, dust_model
     
-        return total_model
+        #return total_model
 
 class Cluster():
     """
@@ -236,9 +236,9 @@ class Cluster():
         total_model = sz_model + dust_model
         
         
-        #return total_model, sz_model, dust_model, SZ_params, I_dust
+        return total_model, sz_model, dust_model, SZ_params, I_dust
         
-        return total_model
+        #return total_model
 
     def initialize(self, theta):
 
