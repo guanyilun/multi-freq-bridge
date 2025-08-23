Fix sign assignment for SZ_params.muc in Filament and Cluster classes; update job configuration for case45

src/model.py
@@ -59,9 +59,9 @@ class Filament():
         SZ_params.betac = np.abs(self.fil_v_avg) * 1000 / const_c
 
         if self.fil_v_avg < 0:
-            SZ_params.muc = -1
-        else:
             SZ_params.muc = 1
+        else:
+            SZ_params.muc = -1
 
         # # Set higher order terms to zero
         SZ_params.means_assign_omegas(0, 0, 0)
@@ -169,9 +169,9 @@ class Cluster():
         SZ_params.betac = np.abs(vc) * 1000 / const_c
 
         if vc < 0:
-            SZ_params.muc = -1
-        else:
             SZ_params.muc = 1
+        else:
+            SZ_params.muc = -1
         
         # # Set higher order terms to zero
         SZ_params.means_assign_omegas(0, 0, 0)