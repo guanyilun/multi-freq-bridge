357f55631e4edec59f8a56f793c30eb67beeb908
Ajay <gillajay.mit.edu>
2025-09-12 20:14:24 -0400

    Fix formatting of FFT model calculation in lnlike function

--- run_mcmc.py
+++ run_mcmc.py
@@ -233,7 +233,7 @@ def lnlike(theta):
                                 z=cf['fil_z'],
                                 muo=cf['fil_muo'])
 
-        fit_model = np.fft.fft2((c1_model + c2_model + fil_model) * apod_mask) * beam_list_array[idx] / mean_apod_mask)
+        fit_model = np.fft.fft2((c1_model + c2_model + fil_model) * apod_mask) * beam_list_array[idx] / mean_apod_mask
 
         resid_loop = data_list_array[idx].ravel() - fit_model.ravel()
         
